2a0e266b07895cdee7302af90d3e54b64d0ff3cf
Chase Hashiguchi <chashiguchi@bsd220.org>
2023-11-13 09:50:22 -0600

    Created tap gesture recognizer

--- Three Letter Word/ContentView.swift
+++ Three Letter Word/ContentView.swift
@@ -15,7 +15,17 @@ struct ContentView: View {
         VStack {
             Text("Three Letter Word")
                 .font(.title).bold()
+                .padding()
             Text("Tap the gray box to change the letter")
+            CustomLetterBox(color: .gray, text: letter)
+                .onTapGesture {
+                    let position = alphabet.index(alphabet.startIndex, offsetBy: counter)
+                    letter = String(alphabet[position])
+                    counter += 1
+                    if counter == alphabet.count {
+                        counter = 0
+                    }
+                }
         }
         .padding()
     }
