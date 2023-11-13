ef70efc6870925a1bd41fbd8b16eb18b4c9226fd
Chase Hashiguchi <chashiguchi@bsd220.org>
2023-11-13 09:32:06 -0600

    Initial Commit

---
+++ Three Letter Word/Three_Letter_WordApp.swift
@@ -0,0 +1,17 @@
+//
+//  Three_Letter_WordApp.swift
+//  Three Letter Word
+//
+//  Created by chase Hashiguchi on 11/13/23.
+//
+
+import SwiftUI
+
+@main
+struct Three_Letter_WordApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
